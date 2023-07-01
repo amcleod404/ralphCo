d94dd394c4244209989242581ab9e874d8a4c865
Andrew <ammcleod2000@gmail.com>
2023-07-01 14:35:41 -0400

    adding cart functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container, Badge } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import { LinkContainer } from 'react-router-bootstrap'
+import { useSelector } from 'react-redux'
 import logo from '../assets/logo2.png'
 
 const Header = () => {
+    const { cartItems } = useSelector((state) => state.cart)
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -18,7 +20,11 @@ const Header = () => {
                 <Navbar.Collapse>
                     <Nav className="ms-auto">
                         <LinkContainer to="/cart">
-                            <Nav.Link><FaShoppingCart/> Cart</Nav.Link>
+                            <Nav.Link><FaShoppingCart style={{marginRight:'5px'}}/>Cart {cartItems.length > 0 && (
+                                <Badge pill bg='success' style={{marginLeft: '5px'}}>
+                                    { cartItems.reduce((acc, curr) => acc + curr.qty, 0) }
+                                </Badge>
+                            ) }</Nav.Link>
                         </LinkContainer>
                         <LinkContainer to="/login">
                             <Nav.Link><FaUser/> Sign In</Nav.Link>
